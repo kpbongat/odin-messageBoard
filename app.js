bcd4b6765fb4087555b43a80567feae272485918
kpbongat <kpbongat.dev@gmail.com>
2024-12-16 14:37:52 +0800

    Create message template and render on index path

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+const path = require("path");
 const express = require("express");
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
 
 const app = express();
 
-const indexRouter = require("./routes/indexRouter");
-const newRouter = require("./routes/newRouter");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use("/", indexRouter);
 app.use("/new", newRouter);
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,18 @@
 const { Router } = require("express");
-
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("Index Page"));
+indexRouter.get("/", (req, res) => res.render("message", { messages }));
+
 module.exports = indexRouter;
